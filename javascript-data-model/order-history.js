9b366b95146f0651ca33baeb0c71051896db0dba
Alex Gonzalez <itsgonzalezalex@gmail.com>
2022-10-18 17:30:09 +0000

    completed javascript-data-model

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,68 @@
+var orderHistory = [
+  {
+    orderNumber: '114-3941689-8772232',
+    total: '$34',
+    orderPlaced: 'August 4, 2020',
+    shipTo: 'JS Masher',
+    delivered: 'Aug 8, 2020',
+    items: [
+      {
+        name: 'Javascript for impatient programmers',
+        author: 'Rauschmayer, Dr. Axel',
+        price: '$31.55',
+        lastReturnDay: 'Sep 7, 2020'
+      }
+    ]
+  },
+  {
+    orderNumber: '113-9984268-1280257',
+    total: '$44.53',
+    orderPlaced: 'July 19, 2020',
+    shipTo: 'JS Masher',
+    delivered: 'Jul 20, 2020',
+    items: [
+      {
+        name: 'The Timeless Way of Building',
+        author: 'Alexander, Christopher',
+        price: '$41.33',
+        lastReturnDay: 'Aug 19, 2020'
+      }
+    ]
+  },
+  {
+    orderNumber: '114-2875557-9059409',
+    total: '$17.22',
+    orderPlaced: 'July 4, 2020',
+    shipTo: 'JS Masher',
+    delivered: 'Jul 7, 2020',
+    items: [
+      {
+        name: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+        author: null,
+        price: '$15.98',
+        lastReturnDay: 'Aug 5, 2020'
+      }
+    ]
+  },
+  {
+    orderNumber: '113-2883177-2648248',
+    total: '$138.93',
+    orderPlaced: 'July 3, 2020',
+    shipTo: 'JS Masher',
+    delivered: 'Jul 5, 2020',
+    items: [
+      {
+        name: 'Gamecube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+        author: null,
+        price: '$94.95',
+        lastReturnDay: 'Aug 4, 2020'
+      },
+      {
+        name: 'The Art of Sql',
+        author: 'Faroult, Stephen',
+        price: '$33.99',
+        lastReturnDay: 'Aug 4, 2020'
+      }
+    ]
+  }
+];

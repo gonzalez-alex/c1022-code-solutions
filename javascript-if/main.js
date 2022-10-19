17c751dc8f5277a9991723e28c4ead1096d41659
Alex Gonzalez <itsgonzalezalex@gmail.com>
2022-10-19 21:42:32 +0000

    updated if statements to have a false value

--- javascript-if/main.js
+++ javascript-if/main.js
@@ -10,30 +10,40 @@
 function isUnderFive(number) {
   if (number < 5) {
     return true;
+  } else {
+    return false;
   }
 }
 
 function isEven(number) {
   if (number % 2 === 0) {
     return true;
+  } else {
+    return false;
   }
 }
 
 function startsWithJ(string) {
   if (string[0] === 'J') {
     return true;
+  } else {
+    return false;
   }
 }
 
 function isOldEnoughToDrink(person) {
   if (person.age >= 21) {
     return true;
+  } else {
+    return false;
   }
 }
 
 function isOldEnoughToDrive(person) {
   if (person.age >= 16) {
     return true;
+  } else {
+    return false;
   }
 }
 
